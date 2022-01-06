3cd8c61a0d54432b7c02f2663b11202befac3296
Joseph Jojy <joseph.jojy61@gmail.com>
2022-01-06 21:49:05 +0530

    Fixed issue

--- src/components/ProductView.jsx
+++ src/components/ProductView.jsx
@@ -9,15 +9,16 @@ const ProductView = () => {
     const { category } = useParams()
 
 
-    const fetchProducts = async() => {
-        try{
-            const response = await axios.get(`${URL}/category/${category}`)
-            setProducts(response.data)
-        }catch(error){console.log(error)}
-    }
+   
     useEffect(() => {
+        const fetchProducts = async() => {
+            try{
+                const response = await axios.get(`${URL}/category/${category}`)
+                setProducts(response.data)
+            }catch(error){console.log(error)}
+        }
         fetchProducts();
-    }, [category,fetchProducts])
+    }, [category])
 
     if (!category)
         return (<div className='text-center w-full my-4 text-lg text-gray-700'>🡸Select a category to shop from.</div>)
