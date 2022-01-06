54f88943653f10490da61e5ccf6b2427709c0f11
Joseph Jojy <joseph.jojy61@gmail.com>
2022-01-07 01:51:13 +0530

    added alt

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -6,7 +6,7 @@ const Cart = ({ cartItems, handleRemove }) => {
         <div className='absolute top-12 right-8 border-2 p-2 w-96 bg-white text-black shadow-lg space-y-2'>
             {cartItems.length ? cartItems.map((item, index) => (
                 <div className='flex flex-row  border-2 p-2' key={index}>
-                    <div className=' h-20'><img src={item.image} className='max-h-full max-w-xs' /></div>
+                    <div className=' h-20'><img src={item.image} className='max-h-full max-w-xs' alt={item.title}/></div>
                     <div className='flex flex-col'>
                         <div className=''>{item.title}</div>
                         <div className='flex justify-end'><button className='bg-red-400 p-1 rounded-md font-bold' onClick={() => handleRemove(index)}>Remove</button></div>
